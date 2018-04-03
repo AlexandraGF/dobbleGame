b67d5a1b06feb1996a3bbccf0a59d21a2a0f2758
Alexy <alexandra.fina@yahoo.com>
2018-04-03 20:36:52 +0100

    Player 1 and 2 can play a game and play again by resetting the game

--- jasmine/interface.js
+++ jasmine/interface.js
@@ -6,6 +6,11 @@ $(document).ready(function() {
     game.randomCard();
     $('#gamecard').attr('src', game.currentCard);
     }
+    if (game.totalCardsNo < 1) {
+      $('#gamecard').attr('src', 'public/dobblegame.jpg');
+      game.resetGame();
+      console.log(game.totalCardsNo);
+    }
   });
 
   $('#player1card').on('click', function() {
@@ -20,4 +25,19 @@ $(document).ready(function() {
     console.log(game.currentCard);
     console.log(game.totalCardsNo);
   });
+
+  $('#player2card').on('click', function() {
+    game.findMatch(game.currentCard, 2);
+    $('#player2card').attr('src', game.currentCard);
+    console.log(game.currentCard);
+    game.randomCard();
+    $('#gamecard').attr('src', game.currentCard);
+    if (game.totalCardsNo == 0) {
+      $('#gamecard').attr('src', 'public/playagain.jpg');
+    }
+    console.log(game.currentCard);
+    console.log(game.totalCardsNo);
+  });
+
+
 });
