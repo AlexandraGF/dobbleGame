4b30fd4e1b1d44f190a9ce162009e930fcf409be
Alexy <alexandra.fina@yahoo.com>
2018-04-03 21:21:37 +0100

    Refactor interface.js

--- jasmine/interface.js
+++ jasmine/interface.js
@@ -9,14 +9,15 @@ $(document).ready(function() {
     if (game.totalCardsNo < 1) {
       $('#gamecard').attr('src', 'public/dobblegame.jpg');
       game.resetGame();
-      console.log(game.totalCardsNo);
+      $('#player1cardswon').text(game.player1CardNo);
+      $('#player2cardswon').text(game.player2CardNo);
+      $('#totalcards').text(game.totalCardsNo);
     }
   });
 
   $('#player1card').on('click', function() {
     game.findMatch(game.currentCard, 1);
     $('#player1card').attr('src', game.currentCard);
-    console.log(game.currentCard);
     game.randomCard();
     $('#gamecard').attr('src', game.currentCard);
     if (game.totalCardsNo == 0) {
@@ -24,14 +25,11 @@ $(document).ready(function() {
     }
     $('#player1cardswon').text(game.player1CardNo);
     $('#totalcards').text(game.totalCardsNo);
-    console.log(game.currentCard);
-    console.log(game.totalCardsNo);
   });
 
   $('#player2card').on('click', function() {
     game.findMatch(game.currentCard, 2);
     $('#player2card').attr('src', game.currentCard);
-    console.log(game.currentCard);
     game.randomCard();
     $('#gamecard').attr('src', game.currentCard);
     if (game.totalCardsNo == 0) {
@@ -39,8 +37,6 @@ $(document).ready(function() {
     }
     $('#player2cardswon').text(game.player2CardNo);
     $('#totalcards').text(game.totalCardsNo);
-    console.log(game.currentCard);
-    console.log(game.totalCardsNo);
   });
 
 
